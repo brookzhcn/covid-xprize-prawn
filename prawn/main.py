c3ad1ac957d0b5f13c514df280d06d804f9584c9
brookzhcn <brookzhcn@gmail.com>
2021-02-02 00:36:19 +0800

    final

--- prawn/main.py
+++ prawn/main.py
@@ -2,7 +2,7 @@ from prawn.prawn_prescribe import run_geo, add_geo_id
 from joblib import Parallel, delayed
 from prawn.standard_predictor.xprize_predictor import NPI_COLUMNS, XPrizePredictor
 import pandas as pd
-
+import time
 
 if __name__ == '__main__':
     path_to_prior_ips_file = 'data/2020-09-30_historical_ip.csv'
@@ -10,10 +10,13 @@ if __name__ == '__main__':
     cost_df = pd.read_csv(path_to_cost_file, dtype={"RegionName": str, "RegionCode": str})
     add_geo_id(cost_df)
     geo_list = cost_df.GeoID.unique().tolist()
-    start_date_str = '2020-08-01'
-    end_date_str = '2020-08-10'
+    start_date_str = '2020-06-01'
+    end_date_str = '2020-08-30'
+    s = time.time()
     outputs = Parallel(backend='loky', n_jobs=12)(delayed(run_geo)(geo, start_date_str, end_date_str,
                                                                    path_to_cost_file, path_to_prior_ips_file)
                                                   for geo in geo_list)
     df = pd.concat(outputs)
     df.to_csv('result.csv', index=False)
+    e = time.time()
+    print(f'Total seconds {e-s}')
